a9152514798da184ce3c355531d04a0a00ea0a8e
deftthunk <35551306+deftthunk@users.noreply.github.com>
2019-01-03 01:47:34 -0500

    updates

--- x265/build.sh
+++ x265/build.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+## grab everything needed for the build environment
 apt-get update
 apt-get install -y --no-install-recommends mercurial cmake cmake-curses-gui build-essential gcc-arm-linux-gnueabi g++-arm-linux-gnueabi ca-certificates
+
+## go to directory where script is located
+parentDir="$(dirname "$0")"
+cd $parentDir
 mkdir src
 cd src
+
+## download and compile x265 source
 hg clone https://bitbucket.org/multicoreware/x265
 cd ./x265/build/linux
 cmake -G "Unix Makefiles" "../../source"
-#./make-Makefiles.bash &
 make
-cd ../../../..
+
+## copy the binary to our mount dir, and exit
+cd $parentDir
 cp ./src/x265/build/linux/x265 .
 rm -rf ./src
+chmod 777 x265
+
+exit 0
